d9a85662700f790b540c64e86017d8fdb7d00442
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-03-25 21:26:10 -0300

    Exercicio redux, implementando redux-thunk

---
+++ exercises/exer-redux-thunk-16_4/doguinhos-app/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import reducerImageDog from './reducerImageDog';
+
+const rootReducer = combineReducers({ reducerImageDog });
+
+export default rootReducer;
